b114f9b0ed97727f386c8e04de6bfc05bbdf0239
ArtashesA <Artashes.Arzumanyan@Evoca.am>
2022-09-15 17:23:29 +0400

    UI commit

--- ui/src/ToDo.js
+++ ui/src/ToDo.js
@@ -1,9 +1,16 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import moment from "moment";
 
 const ToDo = () => {
   const [value, setValue] = useState("");
   const [data, setData] = useState([]);
+  const [search, setSearch] = useState("");
+  const [isUpdate, setIsUpdate] = useState({
+    isUp: false,
+    id: 0,
+  });
+  const [update, setUpdate] = useState("");
 
   const baseUrl = "http://192.168.118.108:10100";
 
@@ -11,7 +18,11 @@ const ToDo = () => {
     axios.get(`${baseUrl}/Todo/GetAll`).then((res) => setData(res.data.data));
   }, []);
 
-  console.log(data);
+  useEffect(() => {
+    axios
+      .get(`${baseUrl}/Todo/Find/${search}`)
+      .then((res) => setData(res.data.data));
+  }, [search]);
 
   const onCreate = () => {
     if (value) {
@@ -32,8 +43,17 @@ const ToDo = () => {
           type="text"
           value={value}
           onChange={(e) => setValue(e.target.value)}
+          placeholder="Add ToDo"
         />
-        <button onClick={onCreate}>Create</button>
+        <button onClick={onCreate} style={{ marginLeft: 5, marginRight: 5 }}>
+          Create
+        </button>
+        <input
+          type="text"
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+          placeholder="Search"
+        />
       </div>
       <table>
         <thead>
@@ -49,9 +69,56 @@ const ToDo = () => {
           {data.map((todo) => (
             <tr>
               <td>{todo.data.id}</td>
-              <td>{todo.data.description}</td>
-              <td>Created Date</td>
-              <td>{}</td>
+              <td
+                onClick={() =>
+                  setIsUpdate({
+                    isUp: true,
+                    id: todo.data.id,
+                  })
+                }
+              >
+                {isUpdate.isUp && isUpdate.id === todo.data.id ? (
+                  <>
+                    <input
+                      value={update}
+                      onChange={(e) => setUpdate(e.target.value)}
+                    />
+                    <button
+                      onClick={() => {
+                        axios
+                          .post(`${baseUrl}/Todo/Update`, {
+                            id: todo.data.id,
+                            Description: update,
+                            isComplete: todo.data.isComplete,
+                          })
+                          .then(() => {
+                            setIsUpdate({
+                              isUp: false,
+                              id: 0,
+                            });
+                            axios
+                              .get(`${baseUrl}/Todo/GetAll`)
+                              .then((res) => setData(res.data.data));
+                          });
+                      }}
+                    >
+                      Update
+                    </button>
+                  </>
+                ) : (
+                  todo.data.description
+                )}
+              </td>
+              <td>
+                {todo.data.createdDate
+                  ? moment(todo.data.createdDate).format("DD/mm/yyyy MM:SS")
+                  : "__"}
+              </td>
+              <td>
+                {todo.data.createdDate
+                  ? moment(todo.data.modifiedDate).format("DD/mm/yyyy MM:SS")
+                  : "__"}
+              </td>
               <td>
                 <input
                   type="checkbox"
